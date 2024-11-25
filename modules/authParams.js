08e581a8e816561e3b11f4d01c8c8b55b97cd840
melissa <melissa.ourabah@tech-instinct.com>
2024-11-25 11:46:15 +0100

    feat(globalà: add auth & api key

---
+++ modules/authParams.js
@@ -0,0 +1,8 @@
+export const pa = {
+    'navbar':d3.select('#navbarMain'),
+    mail:'samszon@gmail.com',
+    apiOmk:'http://localhost/omk_thyp_24-25/api/',
+    ident: 'q7F0p5IgtLLh1wyCGTyGhPTiXljTfg0N',
+    key:'3N99s5KDa591a2SfiQLDC3gXYcC2rvsp',
+    keyGitHub:''
+}
